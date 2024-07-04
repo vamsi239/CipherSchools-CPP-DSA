cac3b41f1251763c0bb7e272115932c020f6a123
vamsi239 <158556635+vamsi239@users.noreply.github.com>
2024-07-04 22:58:21 +0530

    Add files via upload

---
+++ Lecture7-CipherSchools.cpp
@@ -0,0 +1,35 @@
+//1.Array:-
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;  //variable name x of type int 
+    
+    int a[5];   
+
+        // elements starting from 0-5
+        // The first element will have an index 0 --> a[0]
+        // The second element will have an index 1 --> a[1]
+
+        //And goes till a[5]
+
+        //Arrays are of two types:
+        //1.One Dimensional Arrays
+        //2.Two Dimensional Arrays
+}
+
+//2. Reverse given integers:- 
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d,e;
+    
+    cin>>a>>b>>c>>d>>e;
+
+    cout<<"The integers in reverse order are: "<<e<<" "<<d<<" "<<c<<" "<<b<<" "<<a;
+//The integers are being printed in reverse order.
+    return 0;
+}
